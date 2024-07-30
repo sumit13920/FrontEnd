d5e513d548db7916f8504fe203e3f5c5cf655cf0
Sumit Kumar Thakur <140518744+sumit13920@users.noreply.github.com>
2024-07-30 23:57:18 +0000

    datatype-summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+//# primitive
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt,
+
+// const score = 100;
+// const scoreValue = 100.3;
+
+// const isLoggedIn = false;
+// const outsideTemp = null;//object
+// let userEmail;
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+//const bigNumber = 321456987789654n //bigint
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+// const heros = ["shaktiman","naagraj","doga"];
+// let myObj = {
+//     name: "SitaRam",
+//     age: 22,
+// }//Object
+// const myFunction = function() {
+//     console.log("Hello World!");
+// }//Function
+
+// console.log(typeof heros);
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
