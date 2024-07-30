f35015dcc31181d659cb4344f4b1c1196169e2b6
Sumit Kumar Thakur <140518744+sumit13920@users.noreply.github.com>
2024-07-30 23:14:19 +0000

    Add Conversions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);//F
+// console.log(null === 0);//F
+// console.log(null == 0);//F
+// console.log(null >= 0);//T
+
+// console.log(undefined == 0);//F
+// console.log(undefined > 0);//F
+// console.log(undefined < 0);//F
+
+console.log("2" === 2);
