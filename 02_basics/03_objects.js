eaae11b1de27054ceab5a8795e84f7220d9e1d65
Sumit <sumitthakur6500@gmail.com>
2024-08-01 17:45:26 +0530

    concepts dicsussed further

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -19,8 +19,23 @@ const JsUser = {
 }
 
 //console.log(JsUser);
-console.log(JsUser.email)
-console.log(JsUser["location"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym])
-console.log( typeof JsUser[mySym]);
+// console.log(JsUser.email)
+// console.log(JsUser["location"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+// console.log( typeof JsUser[mySym]);
+
+JsUser.email = "SitaRam123@chatgpt.com"
+//Object.freeze(JsUser);
+JsUser.email = "SitaRam123@chatgpt.com"
+// console.log(JsUser)
+
+JsUser.greeting = function() {
+    console.log("Hello JS user")
+}
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS user, ${this.name}`)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
